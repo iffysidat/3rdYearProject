10d92e3170aee8c9fa8fa8c6c5f45ce4adbb7e61
Irfan Sidat <irfanesidat@gmail.com>
2018-12-12 13:33:04 +0000

    Merge branch 'master' of C:\Users\Irfan\PycharmProjects\3rdYearProject with conflicts.

--- SVM/SVMRegressor.py
+++ SVM/SVMRegressor.py
@@ -1,14 +1,11 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import pandas as pd
-
 
 #Function to read data
 def addTrainingLables(filename):
@@ -44,16 +41,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, actualValues, test_size=0
 # X_test = X[split:]
 # y_train = actualValues[:split]
 # y_test = actualValues[split:]
-#
+
 #Preprocess and fit data using scalar
-scalar = MinMaxScaler()
+scalar = StandardScaler()
 scalar.fit(X_train)
 
 X_train = scalar.transform(X_train)
 X_test = scalar.transform(X_test)
 
 from sklearn.svm import SVR
-svr = SVR(kernel="poly")
+svr = SVR(kernel="poly", )
 svr.fit(X_train, y_train)
 
 y_predict = svr.predict(X_test)
@@ -66,4 +63,3 @@ print(y_test)
 print("RMSE")
 rms = sqrt(mean_squared_error(y_test, y_predict))
 print(rms)
-
